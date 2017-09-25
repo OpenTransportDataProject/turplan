35ca0aa1fc6c5110af46202861049420648162ab
Trym Nilsen <trym.o.nilsen@gmail.com>
2017-09-25 12:18:39 +0200

    Fix: Change name to the standard convention
    Large letter on component folders
    index.js file for easier import of component

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import ReactLeafletMap from './components/map/Map.js';
+import ReactLeafletMap from './components/Map';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>varde</h1>
-      <div>
-        <ReactLeafletMap>
-        </ReactLeafletMap>
-        </div>
+      <ReactLeafletMap/>
       </div>
 
     );
