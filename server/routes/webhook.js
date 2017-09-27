c9eb449f659c0927cd80307e5b1802e6778a5728
Sondre Havellen <sondre.havellen@gmail.com>
2017-09-27 13:27:39 +0200

    Even more code

--- server/routes/webhook.js
+++ server/routes/webhook.js
@@ -2,23 +2,24 @@ var express = require('express');
 var router = express.Router();
 var gad = require('node-auto-deploy');
 
-var duration = 5000;
+var duration = 10000;
 
 router.post('/',function(req,res){
 	console.log('Recieved new pull request... TODO: Secret key!');
 	
 	var secret = "gAOP847Z7IEyEyeQuCDQ";
 	console.log('Recieved new pull request...');
-	console.log(req);
+	//console.log(req);
 	if(req.body.secret == secret){
-		console.log('Deploying....');
 		setTimeout(function(){
+			console.log('Deploying....');
 			gad.deploy()
 		},duration);
 		res.send('Success!');
 	} else {
 		console.error('Secret key not valid! Deploying anyway.');
 		setTimeout(function(){
+			console.log('Deploying....');
 			gad.deploy()
 		},duration);
 		res.send('Error: Secret key invalid!');
