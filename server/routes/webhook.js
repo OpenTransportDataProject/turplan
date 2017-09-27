74f297075e3d45ce326a25c9df442b41f0f5f721
Sondre Havellen <sondre.havellen@gmail.com>
2017-09-27 13:11:06 +0200

    New test

--- server/routes/webhook.js
+++ server/routes/webhook.js
@@ -4,16 +4,17 @@ var gad = require('node-auto-deploy');
 
 router.post('/',function(req,res){
 	console.log('Recieved new pull request... TODO: Secret key!');
-	gad.deploy();
-  /*var secret = "gAOP847Z7IEyEyeQuCDQ";
-  console.log('Recieved new pull request...');
-  console.log(req);
-  if(req.body.secret == secret){
-    console.log('Deploying....');
-    gad.deploy();
-  } else {
-    console.error('Secret key not valid!');
-  }*/
+	
+	var secret = "gAOP847Z7IEyEyeQuCDQ";
+	console.log('Recieved new pull request...');
+	console.log(req);
+	if(req.body.secret == secret){
+		console.log('Deploying....');
+		gad.deploy();
+	} else {
+		console.error('Secret key not valid! Deploying anyway.');
+		gad.deploy();
+	}
 });
 
 module.exports = router;
