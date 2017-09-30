f2bdebc5f59e55b6b23096c26fe9a525abd987eb
helemork <helemork@gmail.com>
2017-09-30 22:06:02 +0200

    Marker doesnt show up, but coordinates correct

--- client/src/components/map/Map.js
+++ client/src/components/map/Map.js
@@ -1,7 +1,7 @@
 
 import React, { PureComponent } from 'react';
 import Leaflet from 'leaflet';
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker } from 'react-leaflet';
 import Menubar from '../menubar/Menubar.js';
 
 Leaflet.Icon.Default.imagePath = '//cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.0/images/';
@@ -13,8 +13,9 @@ class ReactLeafletMap extends PureComponent {
       lat: 63.417993,
       lng: 10.405758,
       zoom: 15,
+      markers:[],
     }
-      this.findParkingLots = this.findParkingLots.bind(this);
+    this.findParkingLots = this.findParkingLots.bind(this);
   }
 
   /* This function is connected to the button in the menu, and will use the
@@ -39,7 +40,16 @@ class ReactLeafletMap extends PureComponent {
       const q = '[out:json];node(' +southWest_lat+','+southWest_lng+','+northEast_lat+','+northEast_lng+')[amenity=parking];out;';
       console.log(q);
       overpass(q, (error, data) => {
-        console.log(data)
+        // Here is what gets returned from the api call to overpass based on bounds.
+        var parkingLots = data.features;
+        // Obtaining coordinates for each parking lot entry
+        for (var i=0; i < parkingLots.length;i++){
+          var parkingLot = parkingLots[i];
+          var lat = parkingLot.geometry.coordinates[0];
+          var lng = parkingLot.geometry.coordinates[1];
+          this.setState({markers:[{lat:lat,lng:lng}]});
+        }
+        console.log(data);
       })
     }
 
@@ -51,6 +61,11 @@ class ReactLeafletMap extends PureComponent {
   */
 
   render() {
+    let markers = this.state.markers.map((element, i) => {
+      let NewMarker = (
+        <Marker position={[element.lat, element.lng]} key={i}></Marker>)
+      return (NewMarker)
+    })
     return (
       <div>
         <div className="map">
@@ -58,11 +73,14 @@ class ReactLeafletMap extends PureComponent {
             center={[this.state.lat, this.state.lng]}
             zoom={this.state.zoom}
             ref='map'
+
           >
             <TileLayer
               url="http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=topo2&zoom={z}&x={x}&y={y}"
               attribution='&copy; <a href="http://www.statkart.no">Startkart.no</a>'
             />
+            {markers}
+
           </Map>
         </div>
         <Menubar findParkingLots={this.findParkingLots}/>
