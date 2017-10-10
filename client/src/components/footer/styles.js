5b4d2c43e0ae099322f6ee9391fe3bf8764f8191
Trym Nilsen <trym.o.nilsen@gmail.com>
2017-10-10 10:43:04 +0200

    Style: moved styles into separate file

---
+++ client/src/components/footer/styles.js
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+
+export const FooterContainer = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  background-color: rgb(58, 77, 109);
+  padding-left: 10px;
+  padding-right: 10px;
+`;
+
+export const FooterText = styled.p`
+  font-size: 1em;
+  color: black;
+  padding: 10px;
+`;
+
+export const CustomerContainer = styled.div`justify-content: center;`;
+
+export const NTNUImg = styled.img`
+  height: 50px;
+  padding-right: 100px;
+`;
+
+export const SINTEFImg = styled.img`
+  height: 50px;
+  padding: 10px;
+`;
+
+export const VegvesenImg = styled.img`
+  height: 50px;
+  padding: 10px;
+`;
