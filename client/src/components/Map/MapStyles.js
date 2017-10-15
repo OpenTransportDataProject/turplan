8b965b5afce0e5ad3f958ac775e63231e6a46f5e
Trym Nilsen <trym.o.nilsen@gmail.com>
2017-10-15 12:47:49 +0200

    Move styled components into separate file

---
+++ client/src/components/Map/MapStyles.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex: 1;
+  margin: 1em;
+`;
+
+export const Searchcontainer = styled.div`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const MapContainer = styled.div`
+  flex: 4;
+  justify-content: center;
+  align-items: center;
+`;
