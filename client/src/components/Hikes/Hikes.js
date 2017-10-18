9c8b4a3de6532e5d0b50154f659c3d024cc6e10b
Thomas Skarshaug <thomskar@stud.ntnu.no>
2017-10-18 15:27:37 +0200

    Made the Hikes component

---
+++ client/src/components/Hikes/Hikes.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import axios from "axios";
+
+export class Hikes extends Component {
+
+    constructor(props){
+        super(props);
+
+        
+        // Binding the functions
+        this.getHikes = this.getHikes.bind(this);
+
+    }
+
+
+    
+    async getHikes(bounds){
+        
+                
+                let upperLat = bounds._northEast.lat;
+                let upperLng = bounds._northEast.lng;
+                let lowerLat = bounds._southWest.lat;
+                let lowerLng = bounds._southWest.lng;
+        
+                let url = "http://198.211.120.107:3001";
+        
+                //return axios.get('https://api.github.com/users/' + username + '/repos');
+                // console.log(axios.get(`${url}/api/v1/trips?lat_lower=${lowerLat}&lat_upper=${upperLat}&lng_lower=${lowerLng}&lng_upper=${upperLng}`))
+                
+                let result = await axios.get(`${url}/api/v1/trips?lat_lower=${lowerLat}&lat_upper=${upperLat}&lng_lower=${lowerLng}&lng_upper=${upperLng}`);
+                console.log(result)
+                //return axios.get(`${url}/api/v1/trips?lat_lower=${lowerLat}&lat_upper=${upperLat}&lng_lower=${lowerLng}&lng_upper=${upperLng}`);
+            }
+
+    render(){
+
+
+        return(
+            <div>
+                
+            </div> 
+        )
+    }
+
+}
--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -5,6 +5,7 @@ import Menubar from "../Menubar/Menubar.js";
 import { Searchbar } from "../Searchbar/Searchbar";
 import styled from "styled-components";
 import axios from "axios";
+import {Hikes} from "../Hikes/Hikes";
 
 const Container = styled.div`
 display: flex;
@@ -25,190 +26,8 @@ align-items: center;
 `;
 
 
-//const coordinates = [[63.417993, 10.405758], [66.423132, 10.405758]];
+const coordinates = [[63.417993, 10.405758], [66.423132, 10.405758]];
 
-const coordinates = [
-    [
-        63.1979270275093,
-        10.3030374768782
-    ],
-    [
-        63.1997846592407,
-        10.3032091382617
-    ],
-    [
-        63.2013325946106,
-        10.3032091382617
-    ],
-    [
-        63.2029578376467,
-        10.3045824293297
-    ],
-    [
-        63.2031126179375,
-        10.3080156569997
-    ],
-    [
-        63.2064401939566,
-        10.3129938371212
-    ],
-    [
-        63.2067497164583,
-        10.3164270647912
-    ],
-    [
-        63.2079103963503,
-        10.3205469379952
-    ],
-    [
-        63.2091484035809,
-        10.3244951498165
-    ],
-    [
-        63.210773207678,
-        10.329130007171
-    ],
-    [
-        63.2164206262746,
-        10.3359964625110
-    ],
-    [
-        63.2186638153286,
-        10.339429690181
-    ],
-    [
-        63.222144281475,
-        10.341489626783
-    ],
-    [
-        63.2267842513910,
-        10.3459528227539
-    ],
-    [
-        63.2302637401667,
-        10.3493860504239
-    ],
-    [
-        63.2342066544859,
-        10.3524759553269
-    ],
-    [
-        63.2379171413132,
-        10.3555658602299
-    ],
-    [
-        63.242786432278,
-        10.3591707492834
-    ],
-    [
-        63.2457230681575,
-        10.3629472997204
-    ],
-    [
-        63.247345817805,
-        10.3643205907884
-    ],
-    [
-        63.2490457434626,
-        10.3670671729243
-    ],
-    [
-        63.2522907782543,
-        10.3718736916623
-    ],
-    [
-        63.2539904128626,
-        10.3706720619778
-    ],
-    [
-        63.2556126980303,
-        10.3680971412253
-    ],
-    [
-        63.2570031556275,
-        10.3624323155699
-    ],
-    [
-        63.2580073333577,
-        10.3595140720504
-    ],
-    [
-        63.2587797539982,
-        10.3555658602299
-    ],
-    [
-        63.2611741266482,
-        10.3550508760794
-    ],
-    [
-        63.2624098541839,
-        10.3567674899144
-    ],
-    [
-        63.2641861198351,
-        10.3581407809824
-    ],
-    [
-        63.2665028237985,
-        10.3602007175844
-    ],
-    [
-        63.2688193417981,
-        10.3620889928029
-    ],
-    [
-        63.2707496314167,
-        10.3631189611039
-    ],
-    [
-        63.2739922272877,
-        10.3665521887738
-    ],
-    [
-        63.2763081440804,
-        10.3706720619778
-    ],
-    [
-        63.2804763258049,
-        10.3737619668808
-    ],
-    [
-        63.2827145451786,
-        10.3758219034828
-    ],
-    [
-        63.284335216291,
-        10.3782251628518
-    ],
-    [
-        63.2873447924327,
-        10.3821733746732
-    ],
-    [
-        63.2891966831621,
-        10.3859499251102
-    ],
-    [
-        63.3031560986491,
-        10.3948832089261
-    ],
-    [
-        63.3079372828465,
-        10.4003763731989
-    ],
-    [
-        63.3111756987669,
-        10.4017496642669
-    ],
-    [
-        63.3145679344841,
-        10.4068995057719
-    ],
-    [
-        63.3179597706701,
-        10.4041529236359
-    ]
-]
 
 
 /* This function is connected to the button in the menu, and will use the
@@ -249,7 +68,7 @@ class ReactLeafletMap extends Component {
         this.findParkingLots = this.findParkingLots.bind(this);
         this.findChargingStations = this.findChargingStations.bind(this);
         this.addMarker = this.addMarker.bind(this);
-        this.handleMap = this.handleMap.bind(this);
+        this.moveMap = this.moveMap.bind(this);
         this.logBounds = this.logBounds.bind(this);
         this.getHikes = this.getHikes.bind(this);
 
@@ -374,7 +193,11 @@ class ReactLeafletMap extends Component {
         
     }
 
-    handleMap(lat, lng) {
+    /*
+        This function is being sent as a prop to search bar, which then sets the lat and lng parameters
+        according to the result of the search.
+    */
+    moveMap(lat, lng) {
         this.setState({
             lat,
             lng
@@ -425,7 +248,7 @@ class ReactLeafletMap extends Component {
                 {
 
                     <Searchcontainer>
-                        <Searchbar handleMap={(lat, lng) => this.handleMap(lat, lng)} />
+                        <Searchbar moveMap={(lat, lng) => this.handleMap(lat, lng)} />
                     </Searchcontainer>
 
                 }
@@ -467,6 +290,7 @@ class ReactLeafletMap extends Component {
                     findChargingStations={this.findChargingStations}
                    // logBounds={this.logBounds}
                 />
+                <Hikes />
             </Container>
         );
     }
