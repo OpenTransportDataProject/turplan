c0c9c9372b5c4df459a6d62ebac7cac2bb37bb46
Maliwi95 <marielw@stud.ntnu.no>
2017-10-26 11:04:03 +0200

    Remove unused img in header

--- client/src/components/Header/HeaderStyles.js
+++ client/src/components/Header/HeaderStyles.js
@@ -2,41 +2,31 @@ import styled from "styled-components";
 
 export const HeaderContainer = styled.div`
   flex: 1;
-  background-color: rgb(58, 77, 109,0.4);
-  background-image:url("images/dd.jpg");
-  background-size:cover;
+  background-color: rgb(58, 77, 109, 0.4);
+  background-image: url("images/dd.jpg");
+  background-size: cover;
 `;
 
 export const MainTitle = styled.h1`
   font-size: 50px;
   color: white;
-  text-align:center;
-  @media(max-width:768px){
-    text-align:left;
-    margin-left:10%;
+  text-align: center;
+  @media (max-width: 768px) {
+    text-align: left;
+    margin-left: 10%;
   }
 `;
 
 export const PromoText = styled.div`
   color: white;
-  text-align:left;
-  margin-left:10%;
-  margin-bottom:40px;
-  max-width:50%;
-  font-size:18px;
-  @media(max-width:768px){
-    max-width:80%;
+  text-align: left;
+  margin-left: 10%;
+  margin-bottom: 40px;
+  max-width: 50%;
+  font-size: 18px;
+  @media (max-width: 768px) {
+    max-width: 80%;
   }
-`;
-
-export const LogoImg = styled.img`
-  width: 50px;
-  height: 50px;
-`;
-
-export const SettingsImg = styled.img`
-  width: 32px;
-  height: 32px;
 `;
 
 export const MapButton = styled.button`
@@ -46,7 +36,7 @@ export const MapButton = styled.button`
   background-color: #ddd;
   border: none;
   border-radius: 5px;
-  &:hover{
-    cursor:pointer;
+  &:hover {
+    cursor: pointer;
   }
 `;
